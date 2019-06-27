f11a9100fb21d21f598404a38d9d53964927d990
lxh <lxhnh123>
2019-06-27 08:56:27 +0800

    修改package.json eslint版本： “Known moderate severity security vulnerability detected in eslint < 4.18.2 defined in package.json. package.json update suggested: eslint ~> 4.18.2.”

--- TestiView/src/config/config.js
+++ TestiView/src/config/config.js
@@ -1,10 +1,10 @@
-import Env from './env';
+﻿import Env from './env';
 
 let config = {
     env: Env,
-    jvserver:'http://192.168.1.137:8080',//配置后端服务器地址
+    jvserver:'http://localhost:8080',//配置后端服务器地址
     //jvserver:'http://happydaily.imwork.net:33950',//外网地址，如果当前静态网站通过花生壳让外网访问的话，spring boot的地址也要是外网的。
     //wsserver:'ws://happydaily.imwork.net:33950/ws'//配置后端服务器地址
-    wsserver:'ws://192.168.1.137:8080/ws'//配置后端服务器地址
+    wsserver:'ws://localhost:8080/ws'//配置后端服务器地址
 };
 export default config;
--- TestiViewService/src/main/java/com/june/config/JavaConfig.java
+++ TestiViewService/src/main/java/com/june/config/JavaConfig.java
@@ -177,4 +177,5 @@ public class JavaConfig {
 		return bean;
 	}*/
     
+    
 }
